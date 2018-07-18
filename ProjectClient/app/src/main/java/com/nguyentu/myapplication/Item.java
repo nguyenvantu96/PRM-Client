4ba070b74f6766588ef228d0be88491e4fb1d38f
sonnv10121997 <sonnv10121997@gmail.com>
2018-07-18 22:11:12 +0700

    full business added

--- ProjectClient/app/src/main/java/com/nguyentu/myapplication/Item.java
+++ ProjectClient/app/src/main/java/com/nguyentu/myapplication/Item.java
@@ -3,28 +3,31 @@ package com.nguyentu.myapplication;
 public class Item {
     private String question;
     private String answer;
+    private char[] guessedWord;
+    private int whiteSpaceIdx;
 
-    public Item(String question, String answer) {
+    public Item(String question, String answer, char[] guessedWord) {
         this.question = question;
         this.answer = answer;
+        this.guessedWord = guessedWord;
     }
 
-    public Item() {
-    }
+    public Item() { }
 
-    public String getQuestion() {
-        return question;
-    }
+    public String getQuestion() { return question; }
 
-    public void setQuestion(String question) {
-        this.question = question;
-    }
+    public void setQuestion(String question) { this.question = question; }
 
-    public String getAnswer() {
-        return answer;
-    }
+    public String getAnswer() { return answer; }
 
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
+    public char[] getGuessedWord() { return guessedWord; }
+
+    public void setGuessedWord(char[] guessedWord) { this.guessedWord = guessedWord; }
+
+    public void setAnswer(String answer) { this.answer = answer; }
+
+    public int getWhiteSpaceIdx() { return whiteSpaceIdx; }
+
+    public void setWhiteSpaceIdx(int whiteSpaceIdx) { this.whiteSpaceIdx = whiteSpaceIdx; }
+
 }
--- ProjectClient/app/src/main/java/com/nguyentu/myapplication/MainActivity.java
+++ ProjectClient/app/src/main/java/com/nguyentu/myapplication/MainActivity.java
@@ -1,11 +1,15 @@
 package com.nguyentu.myapplication;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,42 +22,98 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private String TAG = MainActivity.class.getSimpleName();
     TextView tView;
+    TextView question;
     List<Item> list;
     Controller c;
     EditText editText;
     Item current;
+    LinearLayout linearLayout;
+    Button btnStart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tView = findViewById(R.id.tView);
+        question = findViewById(R.id.question);
         list = new ArrayList<>();
         c = new Controller();
         editText = findViewById(R.id.editText);
+        linearLayout = findViewById(R.id.blockQues);
+        btnStart = findViewById(R.id.btnStart);
+        btnStart.setEnabled(false);
         new GetData().execute();
-
     }
-    public void startGame(View view){
 
+    public void startGame(View view) {
+        int idx = 0;
         current = c.randomAnwser(list);
-        Toast.makeText(MainActivity.this, current.getQuestion().toString(), Toast.LENGTH_SHORT).show();
+        linearLayout.removeAllViews();
 
+        for (char character : current.getGuessedWord()) {
+            LinearLayout.LayoutParams params =
+                new LinearLayout.LayoutParams(100, 115);
+
+            if (idx > 0 && (idx - 1) == current.getWhiteSpaceIdx()) {
+                params.setMargins(82, 0, 0, 0);
+            }
+            if (idx == current.getWhiteSpaceIdx()) {
+                idx++;
+                continue;
+            }
+
+            Button button = new Button(this);
+            button.setLayoutParams(params);
+            button.setText(character + "");
+            linearLayout.addView(button);
+            idx++;
+        }
+
+        question.setText(current.getQuestion());
     }
-    public void onClick(View view){
 
-try {
+    public void onClick(View view) {
+        int count, idx = 0;
+        try {
+            String currentAnswer = current.getAnswer();
+            char inputAnswer = editText.getText().toString().toUpperCase().charAt(0);
+            char[] guessedWord = c.checkAnwser(
+                currentAnswer,
+                current.getGuessedWord(),
+                inputAnswer
+            );
+            editText.setText("");
+            current.setGuessedWord(guessedWord);
+            count = c.countCorrectWord(currentAnswer, inputAnswer);
+            if (count > 0){
+                tView.setText("Có " + count + " chữ "+ inputAnswer);
+            } else {
+                tView.setText("Không có chữ " + inputAnswer + " nào");
+            }
 
+            linearLayout.removeAllViews();
+            for (char character : current.getGuessedWord()) {
+                LinearLayout.LayoutParams params =
+                        new LinearLayout.LayoutParams(100, 115);
 
-   int number = c.checkAnwser(current.getAnswer(),editText.getText().toString());
-   if(number > 0){
-       tView.setText("Có" + number + "chữ "+editText.getText().toString());
-   }else {
-       tView.setText("Không có chữ " +editText.getText().toString() + " nào");
-   }
-}catch (Exception e){
-    e.getMessage();
-}
-}
+                if (idx > 0 && (idx - 1) == current.getWhiteSpaceIdx()) {
+                    params.setMargins(82, 0, 0, 0);
+                }
+                if (idx == current.getWhiteSpaceIdx()) {
+                    idx++;
+                    continue;
+                }
+
+                Button button = new Button(this);
+                button.setLayoutParams(params);
+                button.setText(character + "");
+                linearLayout.addView(button);
+                idx++;
+            }
+        } catch (Exception e){
+            e.getMessage();
+        }
+    }
 
     private class GetData extends AsyncTask<Void, Void, Void> {
         @Override
@@ -65,20 +125,23 @@ try {
 
         @Override
         protected Void doInBackground(Void... voids) {
-
             HttpHandler sh = new HttpHandler();
             String url = "http://150.95.105.112";
             String jsonStr =  sh.makeServiceCall(url) ;
             if (jsonStr != null ) {
                 try {
-
                     JSONArray jsonarray = new JSONArray(jsonStr);
                     for (int i = 0; i < jsonarray.length(); i++) {
                         JSONObject c = jsonarray.getJSONObject(i);
                         String question = c.getString("question");
-                        String anwser = c.getString("answer");
-                       Item item = new Item(question,anwser);
-                       list.add(item);
+                        String answer = c.getString("answer").trim().toUpperCase();
+                        char[] guessedWord = new char[answer.length()];
+                        for (int j = 0; j < guessedWord.length; j++) {
+                            guessedWord[j] = ' ';
+                        }
+                        Item item = new Item(question, answer, guessedWord);
+                        item.setWhiteSpaceIdx(answer.indexOf(' '));
+                        list.add(item);
                     }
 
                     runOnUiThread(new Runnable() {
@@ -87,6 +150,7 @@ try {
                             Toast.makeText(getApplicationContext(),
                                     "Data success",
                                     Toast.LENGTH_LONG).show();
+                            btnStart.setEnabled(true);
                         }
                     });
                 } catch (final Exception e) {
